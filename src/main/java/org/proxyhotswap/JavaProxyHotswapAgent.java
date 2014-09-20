5cc530079edc1b75caa9de942c9a7d2cec42b4d0
erkieh <erki.ehtla@gmail.com>
2014-09-20 18:31:51 +0300

    initial version

---
+++ src/main/java/org/proxyhotswap/JavaProxyHotswapAgent.java
@@ -0,0 +1,21 @@
+package org.proxyhotswap;
+
+import java.lang.instrument.Instrumentation;
+
+/**
+ * @author Erki Ehtla
+ * 
+ */
+public class JavaProxyHotswapAgent {
+	public static Instrumentation INSTRUMENTATION;
+	
+	public static void premain(String agentArgs, Instrumentation inst) {
+		inst.addTransformer(new JavaProxyTransformer(inst));
+		INSTRUMENTATION = inst;
+	}
+	
+	public static void agentmain(String agentArgs, Instrumentation inst) {
+		inst.addTransformer(new JavaProxyTransformer(inst));
+		INSTRUMENTATION = inst;
+	}
+}
